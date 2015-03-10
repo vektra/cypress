e3f7438c65f6f0a661e604b68f5e857ee0367e66
Evan Phoenix <evan@phx.io>
2015-03-09 23:10:14 -0700

    Remove SwitchStream and Negotiate, using Decoder and Probe

--- errors.go
+++ errors.go
@@ -2,4 +2,7 @@ package cypress
 
 import "errors"
 
-var ErrInvalidMessage = errors.New("invalid message")
+var (
+	ErrInvalidMessage    = errors.New("invalid message")
+	ErrUnknownStreamType = errors.New("unknown stream type")
+)
