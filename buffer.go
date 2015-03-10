e15e9acf4cad6dd04a36131bff2cf42fd059e022
Evan Phoenix <evan@phx.io>
2015-03-10 09:36:55 -0700

    Use a closure rather than a standalone func

--- buffer.go
+++ buffer.go
@@ -5,9 +5,7 @@ import "sync"
 var pbBufPool sync.Pool
 
 func init() {
-	pbBufPool.New = newBuf
-}
-
-func newBuf() interface{} {
-	return make([]byte, 128)
+	pbBufPool.New = func() interface{} {
+		return make([]byte, 128)
+	}
 }
