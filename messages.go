da5ccabe2680606030f3034e5af4a40725a3f59b
Evan Phoenix <evan@phx.io>
2015-03-20 19:07:07 -0700

    Add missing file

---
+++ messages.go
@@ -0,0 +1,10 @@
+package cypress
+
+type Messages []*Message
+
+func (m Messages) Len() int      { return len(m) }
+func (m Messages) Swap(i, j int) { m[i], m[j] = m[j], m[i] }
+
+func (m Messages) Less(i, j int) bool {
+	return m[i].GetTimestamp().Before(m[j].GetTimestamp())
+}
