b46b13d53aee71909485551b9ace1ff83ffd99b4
Jessica Lynn Suttles <jlsuttles@gmail.com>
2015-03-26 10:47:06 -0700

    Start papertrail generate cli command

--- cli/papertrail.go
+++ cli/papertrail.go
@@ -29,9 +29,44 @@ func (p *PapertrailCommand) Execute(args []string) error {
 	return cypress.Glue(dec, papertrail)
 }
 
+type PapertrailGenerateCommand struct {
+	Token      string `short:"T" long:"token" description:"Papertrail token"`
+	Q          string `short:"q" long:"query" description:"Query string"`
+	GroupId    string `long:"groupid"`
+	SystemId   string `long:"systemid"`
+	MinId      string `long:"minid"`
+	MaxId      string `long:"maxid"`
+	MinTime    string `long:"mintime"`
+	MaxTime    string `long:"maxtime"`
+	BufferSize int    `long:"buffersize" default:"100"`
+}
+
+func (g *PapertrailGenerateCommand) Execute(args []string) error {
+	options := &papertrail.EventsOptions{
+		Q:        g.Q,
+		GroupId:  g.GroupId,
+		SystemId: g.SystemId,
+		MinId:    g.MinId,
+		MaxId:    g.MaxId,
+		MinTime:  g.MinTime,
+		MaxTime:  g.MaxTime,
+	}
+
+	generator := papertrail.NewAPIClient(g.Token, options, g.BufferSize)
+
+	receiver := os.Stdout
+
+	return cypress.Glue(generator, receiver)
+}
+
 func init() {
 	short := "Send messages to Papertrail"
 	long := "Given a stream on stdin, the papertrail command will read those messages in and send them to Papertrail via TCP."
 
 	addCommand("papertrail", short, long, &PapertrailCommand{})
+
+	short = "Get messages from Papertrail"
+	long = ""
+
+	addCommand("papertrail:generate", short, long, &PapertrailGenerateCommand{})
 }
